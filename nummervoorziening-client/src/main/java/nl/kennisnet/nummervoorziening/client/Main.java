e780497cb4dc5ba7c081ad03251b381863716da7
Ruslan Vidert <r2.vidert@itransition.com>
2016-03-05 11:59:23 +0300

    NV-143: Create basic structure of java client.

---
+++ nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/Main.java
@@ -0,0 +1,16 @@
+package nl.kennisnet.nummervoorziening.client;
+
+/**
+ * Main class. Contains program entry point.
+ */
+public class Main {
+
+    /**
+     * The main entry point for the program.
+     *
+     * @param args Command line arguments to the program.
+     */
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+    }
+}
