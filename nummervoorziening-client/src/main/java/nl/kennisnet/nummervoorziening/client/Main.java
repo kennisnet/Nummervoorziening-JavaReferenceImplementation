af27e7d69268d3ac1475a57787df03b9eff34c9e
Ruslan Vidert <r2.vidert@itransition.com>
2016-03-05 12:48:05 +0300

    NV-144: Add generating classes from the wsdl.

--- nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/Main.java
+++ nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/Main.java
@@ -1,5 +1,7 @@
 package nl.kennisnet.nummervoorziening.client;
 
+import school.id.eck.schemas.v1_0.Chain;
+
 /**
  * Main class. Contains program entry point.
  */
@@ -11,6 +13,7 @@ public class Main {
      * @param args Command line arguments to the program.
      */
     public static void main(String args[]) {
+        new Chain(); // This line demonstrates that we can interact with generated from wsdl code here.
         System.out.println("Hello, World!");
     }
 }
