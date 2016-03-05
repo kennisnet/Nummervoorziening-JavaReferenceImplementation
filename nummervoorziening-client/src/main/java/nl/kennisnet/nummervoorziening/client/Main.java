d278fea8f310380ad7bd05b748e190dc1e1f09ba
Ruslan Vidert <r2.vidert@itransition.com>
2016-03-05 16:48:46 +0300

    NV-146: Add util for working with web services, add ping method, update server address in wsdl, add several tests that check ping operation.

--- nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/Main.java
+++ nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/Main.java
@@ -1,6 +1,9 @@
 package nl.kennisnet.nummervoorziening.client;
 
-import school.id.eck.schemas.v1_0.Chain;
+import school.id.eck.schemas.v1_0.PingResponse;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Main class. Contains program entry point.
@@ -10,10 +13,14 @@ public class Main {
     /**
      * The main entry point for the program.
      *
-     * @param args Command line arguments to the program.
+     * @param args Command line arguments to the program. Not Used.
      */
-    public static void main(String args[]) {
-        new Chain(); // This line demonstrates that we can interact with generated from wsdl code here.
-        System.out.println("Hello, World!");
+    public static void main(String args[]) throws NoSuchAlgorithmException, KeyManagementException {
+        System.out.println("Current server information:");
+        SchoolIdServiceUtil schoolIdServiceUtil = new SchoolIdServiceUtil();
+        PingResponse pingResponse = schoolIdServiceUtil.ping();
+        System.out.println("Application version: " + pingResponse.getApplicationVersion());
+        System.out.println("Available:           " + pingResponse.isAvailable());
+        System.out.println("System time:         " + pingResponse.getSystemTime());
     }
 }
---
+++ nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/SchoolIdServiceUtil.java
@@ -0,0 +1,49 @@
+package nl.kennisnet.nummervoorziening.client;
+
+import school.id.eck.schemas.v1_0.PingRequest;
+import school.id.eck.schemas.v1_0.PingResponse;
+import school.id.eck.schemas.v1_0.SchoolID;
+import school.id.eck.schemas.v1_0.SchoolIDService;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Utility class that helps to work with Web Service.
+ */
+public class SchoolIdServiceUtil {
+
+    private final SchoolID schoolID;
+
+    /**
+     * Initializes class for working with SchoolID Web Service.
+     */
+    public SchoolIdServiceUtil() throws NoSuchAlgorithmException, KeyManagementException {
+        SchoolIDService schoolIDService = new SchoolIDService();
+        schoolID = schoolIDService.getSchoolIDSoap10();
+        disableSsl();
+    }
+
+    /**
+     * Executes request to Web Service for getting information about its availability/configuration.
+     *
+     * @return PingResponse instance with information about Web Service.
+     */
+    public PingResponse ping() {
+        return schoolID.ping(new PingRequest());
+    }
+
+    /**
+     * Disables ssl verifications. It is needed in development if you are using
+     * self signed certificate, this method should not be used in production.
+     */
+    private void disableSsl() throws NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(null, new TrustManager[] { new TrustAllX509TrustManager() }, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+    }
+}
