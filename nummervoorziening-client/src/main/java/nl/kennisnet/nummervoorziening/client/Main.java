9fc31074fa1dd178f8a2ac61995258ec2bb59bdd
Ruslan Vidert <r2.vidert@itransition.com>
2016-03-05 19:43:18 +0300

    NV-149: Add method into wrapper for executing "Retrieve ECK" operation. Add test for checking that server returns non empty string.

--- nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/Main.java
+++ nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/Main.java
@@ -1,11 +1,10 @@
 package nl.kennisnet.nummervoorziening.client;
 
-import school.id.eck.schemas.v1_0.PingResponse;
-import school.id.eck.schemas.v1_0.RetrieveChainsResponse;
-import school.id.eck.schemas.v1_0.RetrieveSectorsResponse;
+import school.id.eck.schemas.v1_0.*;
 
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 /**
  * Main class. Contains program entry point.
@@ -15,7 +14,8 @@ public class Main {
     private static final String WEB_SERVICE_APPLICATION_VERSION = "0.1.0-SNAPSHOT";
 
     /**
-     * The main entry point for the program.
+     * The main entry point for the program. This function demonstrates work
+     * with Web Services via created util.
      *
      * @param args Command line arguments to the program. Not Used.
      */
@@ -37,8 +37,19 @@ public class Main {
             return;
         }
         RetrieveChainsResponse retrieveChainsResponse = schoolIdServiceUtil.retrieveChains();
-        System.out.println("Count of active chains:    " + retrieveChainsResponse.getChain().size());
+        List<Chain> activeChains = retrieveChainsResponse.getChain();
+        System.out.println("Count of active chains:    " + activeChains.size());
         RetrieveSectorsResponse retrieveSectorsResponse = schoolIdServiceUtil.retrieveSectors();
-        System.out.println("Count of active sectors:   " + retrieveSectorsResponse.getSector().size());
+        List<Sector> activeSectors = retrieveSectorsResponse.getSector();
+        System.out.println("Count of active sectors:   " + activeSectors.size());
+        System.out.println("Retrieving EckId for first active sector and first active chain:");
+        String chainId = activeChains.get(0).getId();
+        System.out.println("ChainId:    " + chainId);
+        String sectorId = activeSectors.get(0).getId();
+        System.out.println("SectorId:   " + sectorId);
+        String hPgn = "hpgn";
+        System.out.println("HPgn:       " + hPgn);
+        RetrieveEckIdResponse retrieveEckIdResponse = schoolIdServiceUtil.retrieveEckId(chainId, sectorId, hPgn);
+        System.out.println("Retrieved EckID: " + retrieveEckIdResponse.getEckId().getValue());
     }
 }
