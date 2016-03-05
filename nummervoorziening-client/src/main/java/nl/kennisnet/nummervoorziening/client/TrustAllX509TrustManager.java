1326e8f8e5564c58e99ce23ab411f2733b0edb25
Ruslan Vidert <r2.vidert@itransition.com>
2016-03-05 16:48:46 +0300

    NV-146: Add util for working with web services, add ping method, update server address in wsdl, add several tests that check ping operation.

---
+++ nummervoorziening-client/src/main/java/nl/kennisnet/nummervoorziening/client/TrustAllX509TrustManager.java
@@ -0,0 +1,23 @@
+package nl.kennisnet.nummervoorziening.client;
+
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+/**
+ * Provides an implementation of the <code>X509TrustManager</code> which does
+ * not validate the supplied certificate. Do not use this class in production!
+ */
+public class TrustAllX509TrustManager implements X509TrustManager {
+
+    public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+    }
+
+    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+        // do nothing
+    }
+
+    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+        // do nothing
+    }
+}
