f328412f9f965b117e1a81a4065782e451f0639b
Ruslan Vidert <r2.vidert@itransition.com>
2016-03-09 14:22:53 +0300

    NV-156: Change structure of Java client to be similar with C# client.

---
+++ UnitTestProject/src/test/java/nl/kennisnet/nummervoorziening/client/RetrieveEckIdTest.java
@@ -0,0 +1,39 @@
+package nl.kennisnet.nummervoorziening.client;
+
+import nl.kennisnet.nummervoorziening.client.schoolid.SchoolIDServiceUtil;
+import org.junit.Before;
+import org.junit.Test;
+import school.id.eck.schemas.v1_0.RetrieveEckIdResponse;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Demonstrates correct work with "Retrieve EckId" operation.
+ */
+public class RetrieveEckIdTest {
+
+    private SchoolIDServiceUtil schoolIdServiceUtil;
+
+    /**
+     * Setups util for working with Web Service.
+     */
+    @Before
+    public void setup() throws NoSuchAlgorithmException, KeyManagementException {
+        schoolIdServiceUtil = new SchoolIDServiceUtil();
+    }
+
+    /**
+     * Tests that Web Service returns non empty string if we tries to create EckId
+     * for first active chain and sector and 'hpgn' string.
+     */
+    @Test
+    public void testWebServiceReturnsNonEmptyEckId() {
+        String chainId = schoolIdServiceUtil.retrieveChains().getChain().get(0).getId();
+        String sectorId = schoolIdServiceUtil.retrieveSectors().getSector().get(0).getId();
+        RetrieveEckIdResponse retrieveEckIdResponse = schoolIdServiceUtil.retrieveEckId(chainId, sectorId, "hpgn");
+        assertFalse("Web Service returned empty EckId!", retrieveEckIdResponse.getEckId().getValue().isEmpty());
+    }
+}
