daab0a5a76cf5ca957c48135b363fcf3b1e21f50
Thomas Beekman <t.beekman@kennisnet.nl>
2016-03-10 10:32:28 +0100

    - Few adjustments to align Java unit tests with the C# unit tests

--- UnitTestProject/src/test/java/nl/kennisnet/nummervoorziening/client/RetrieveEckIdTest.java
+++ UnitTestProject/src/test/java/nl/kennisnet/nummervoorziening/client/RetrieveEckIdTest.java
@@ -8,7 +8,7 @@ import javax.xml.ws.soap.SOAPFaultException;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Demonstrates correct work with "Retrieve EckId" operation.
+ * Demonstrates correct usage of the "Retrieve EckId" operation.
  */
 public class RetrieveEckIdTest extends AbstractUnitTest {
 
@@ -20,7 +20,7 @@ public class RetrieveEckIdTest extends AbstractUnitTest {
 
     private static  String VALID_SECTOR_GUID = "512e4729-03a4-43a2-95ba-758071d1b725";
 
-    private static final String VALID_EKT_ID = "https://school.id/pilot/998fc3e7c9add25be4369224e18d0876e7598480b184c" +
+    private static final String VALID_SCHOOL_ID = "https://school.id/pilot/998fc3e7c9add25be4369224e18d0876e7598480b184c" +
         "6a35d8f49a49a3649040016f0aab6e292dd7da23292bd2f499e6018dfdab997d9408d80113d6dc72979";
 
     private static final String INVALID_HPGN = "";
@@ -30,7 +30,7 @@ public class RetrieveEckIdTest extends AbstractUnitTest {
     private static final String INVALID_SECTOR_GUID = "invalidsectorguid";
 
     /**
-     * Tests that Web Service throws error on invalid hpgn.
+     * Tests that Nummervoorziening service throws error on invalid HPgn.
      */
     @Test(expected = SOAPFaultException.class)
     public void testGetEckIdWithInvalidHpgn() {
@@ -38,7 +38,7 @@ public class RetrieveEckIdTest extends AbstractUnitTest {
     }
 
     /**
-     * Tests that Web Service throws error on invalid chain guid.
+     * Tests that Nummervoorziening service throws error on invalid Chain Guid.
      */
     @Test(expected = SOAPFaultException.class)
     public void testGetEckIdWithInvalidChain() {
@@ -46,7 +46,7 @@ public class RetrieveEckIdTest extends AbstractUnitTest {
     }
 
     /**
-     * Tests that Web Service throws error on invalid sector guid.
+     * Tests that Nummervoorziening service throws error on invalid Sector Guid.
      */
     @Test(expected = SOAPFaultException.class)
     public void testGetEckIdWithInvalidSector() {
@@ -54,11 +54,11 @@ public class RetrieveEckIdTest extends AbstractUnitTest {
     }
 
     /**
-     * Tests that Web Service returns correct eck_id on valid parameters.
+     * Tests that Nummervoorziening service returns correct SchoolID on valid parameters.
      */
     @Test
     public void testGetEckIdWithValidValues() {
         String schoolId = schoolIdServiceUtil.generateSchoolID(VALID_HPGN, VALID_CHAIN_GUID, VALID_SECTOR_GUID);
-        assertEquals(VALID_EKT_ID, schoolId);
+        assertEquals(VALID_SCHOOL_ID, schoolId);
     }
 }
