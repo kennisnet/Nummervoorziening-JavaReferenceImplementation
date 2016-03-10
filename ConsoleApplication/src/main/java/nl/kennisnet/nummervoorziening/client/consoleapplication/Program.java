a0ae851eceb2ae2363fa6c389731138982fb1313
Thomas Beekman <t.beekman@kennisnet.nl>
2016-03-10 10:04:56 +0100

    - Aligned Program.java and Program.cs

--- ConsoleApplication/src/main/java/nl/kennisnet/nummervoorziening/client/consoleapplication/Program.java
+++ ConsoleApplication/src/main/java/nl/kennisnet/nummervoorziening/client/consoleapplication/Program.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class Program {
 
     private static final String WEB_SERVICE_APPLICATION_VERSION = "0.1.0-SNAPSHOT";
+    private static SchoolIDServiceUtil schoolIDServiceUtil;
 
     /**
      * The main entry point for the program. This function demonstrates work
@@ -24,34 +25,55 @@ public class Program {
      */
     public static void main(String args[]) throws NoSuchAlgorithmException, KeyManagementException {
         System.out.println("Current server information:");
-        SchoolIDServiceUtil schoolIdServiceUtil = new SchoolIDServiceUtil();
-        String applicationVersion = schoolIdServiceUtil.getApplicationVersion();
-        System.out.println("Application version:\t\t" + applicationVersion);
-        if (!WEB_SERVICE_APPLICATION_VERSION.equals(applicationVersion)) {
-            System.out.println("Web Service Application version is different from intended (" +
-                    WEB_SERVICE_APPLICATION_VERSION + "), finishing.");
-            return;
-        }
-        System.out.println("System time:\t\t\t\t" + schoolIdServiceUtil.getSystemTime());
-        System.out.println("Available:\t\t\t\t\t" + schoolIdServiceUtil.isWebServiceAvailable());
-        if (!schoolIdServiceUtil.isWebServiceAvailable()) {
+        schoolIDServiceUtil = new SchoolIDServiceUtil();
+
+        if (!schoolIDServiceUtil.isWebServiceAvailable()) {
             System.out.println("Web Service is not available, finishing.");
-            return;
+        } else {
+            String applicationVersion = schoolIDServiceUtil.getApplicationVersion();
+            System.out.println("Application version:\t\t" + applicationVersion);
+            if (!WEB_SERVICE_APPLICATION_VERSION.equals(applicationVersion)) {
+                System.out.println("Web Service Application version is different from intended (" +
+                    WEB_SERVICE_APPLICATION_VERSION + "), finishing.");
+                return;
+            }
+            System.out.println("System time:\t\t\t\t" + schoolIDServiceUtil.getSystemTime());
+            System.out.println("Available:\t\t\t\t\t" + schoolIDServiceUtil.isWebServiceAvailable());
+
+            List<Chain> activeChains = schoolIDServiceUtil.getChains();
+            System.out.println("Count of active chains:\t\t" + activeChains.size());
+
+            List<Sector> activeSectors = schoolIDServiceUtil.getSectors();
+            System.out.println("Count of active sectors:\t" + activeSectors.size());
+
+            System.out.println("\nRetrieving SchoolID for first active sector and first active chain:");
+            String chainId = activeChains.get(0).getId();
+            System.out.println("ChainId:\t\t\t\t\t" + chainId);
+            String sectorId = activeSectors.get(0).getId();
+            System.out.println("SectorId:\t\t\t\t\t" + sectorId);
+
+            // Execute a number of valid tests
+            executeClientTest("063138219", chainId, sectorId);
+            executeClientTest("teacher@school.com", chainId, sectorId);
         }
-        List<Chain> activeChains = schoolIdServiceUtil.getChains();
-        System.out.println("Count of active chains:\t\t" + activeChains.size());
-        List<Sector> activeSectors = schoolIdServiceUtil.getSectors();
-        System.out.println("Count of active sectors:\t" + activeSectors.size());
-        System.out.println("\nRetrieving EckId for first active sector and first active chain:");
-        String chainId = activeChains.get(0).getId();
-        System.out.println("ChainId:\t\t\t\t\t" + chainId);
-        String sectorId = activeSectors.get(0).getId();
-        System.out.println("SectorId:\t\t\t\t\t" + sectorId);
-        String pgn = "pgn";
+    }
+
+    /**
+     * Executes test cases
+     * @param pgn The PGN input
+     * @param chainGuid A valid Chain Guid
+     * @param sectorGuid A valid Sector Guid
+     */
+    private static void executeClientTest(String pgn, String chainGuid, String sectorGuid) {
         System.out.println("Pgn:\t\t\t\t\t\t" + pgn);
+
+        // Generate scrypt hash of the given PGN
         String hpgn = ScryptUtil.generateHexHash(pgn);
         System.out.println("HPgn:\t\t\t\t\t\t" + hpgn);
-        String eckId = schoolIdServiceUtil.generateSchoolID(hpgn, chainId, sectorId);
-        System.out.println("Retrieved EckID:\t\t\t" + eckId);
+
+        // Retrieve SchoolID from Nummervoorziening service
+        String eckId = schoolIDServiceUtil.generateSchoolID(hpgn, chainGuid, sectorGuid);
+        System.out.println("Retrieved SchoolID:\t\t\t" + eckId);
+        System.out.println();
     }
 }
