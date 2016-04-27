2de32debdda6397ea14a76b21caaa4a12bc2a0ba
Ruslan Vidert <ruslan.vidert@kandasoft.com>
2016-04-27 19:32:23 +0300

    NV-339: Add apache license notice near the top of every source file.

--- ConsoleApplication/src/main/java/nl/kennisnet/nummervoorziening/client/consoleapplication/Program.java
+++ ConsoleApplication/src/main/java/nl/kennisnet/nummervoorziening/client/consoleapplication/Program.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016, Stichting Kennisnet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.kennisnet.nummervoorziening.client.consoleapplication;
 
 import nl.kennisnet.nummervoorziening.client.schoolid.SchoolIDBatch;
@@ -117,7 +132,7 @@ public class Program {
             System.out.println("Exception thrown by service while trying to submit batch: " +
                 e.getFault().getFaultActor());
 
-            switch(e.getFault().getFaultActor()) {
+            switch (e.getFault().getFaultActor()) {
                 case "LimitDailyBatchSubmissionsExceededException":
                     System.out.println("Reached the limit of the amount of allowed daily submissions");
                     break;
